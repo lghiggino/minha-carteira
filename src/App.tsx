261b0f7a94248ef270f4ad27797ec4ade11b8c52
lghiggino <lghiggino@gmail.com>
2021-06-30 21:52:37 -0300

    styled components e base do grid

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Layout from "./components/Layout";
+
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <GlobalStyles />
+            <Layout />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const MainHeader: React.FC = () => {
+    return (
+        <Container>
+            <h1>Aqui é o MainHeader</h1>
+        </Container>
+    )
+}
+
+export default MainHeader
