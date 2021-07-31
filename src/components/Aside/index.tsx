26ae6ca30b1f568d6a516bd561fb6efcec47c502
lghiggino <lghiggino@gmail.com>
2021-07-30 22:18:25 -0300

    testing components behavior

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,6 +6,7 @@ const Aside: React.FC = () => {
     return (
         <Container>
             <h1>Aqui é o Aside</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quo dignissimos optio ipsam ipsa porro velit facere culpa est rerum blanditiis dicta quidem hic, sint et excepturi, maxime non cum?</p>
         </Container>
     )
 }
--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -6,6 +6,7 @@ const Content: React.FC = () => {
     return (
         <Container>
             <h1>Aqui é o Content</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam sapiente reprehenderit. Laborum beatae praesentium totam maxime unde amet recusandae mollitia tempore! Rerum explicabo eaque sed consequatur iste iusto dolore architecto eveniet aperiam pariatur? Nisi dolorum reiciendis harum, inventore perspiciatis ipsam modi? Quibusdam repudiandae ratione unde nostrum maiores! Aliquid, veritatis.</p>
         </Container>
     )
 }
